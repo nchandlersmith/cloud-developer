c4b0d7c23480f3d9f0d5aee15a11d485341928bc
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-11-13 08:35:14 -0600

    refactors

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -11,56 +11,74 @@ import fs from "fs";
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-/*
-  @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  GET /filteredimage?image_url={{URL}}
-  endpoint to filter an image from a public url.
-  IT SHOULD
-     1
-     1. validate the image_url query
-     2. call filterImageFromURL(image_url) to filter the image
-     3. send the resulting file in the response
-     4. deletes any files on the server on finish of the response
-  QUERY PARAMATERS
-     image_url: URL of a publicly accessible image
-  RETURNS
-    the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-  ! END @TODO1
-  */
+  /*
+    @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+    GET /filteredimage?image_url={{URL}}
+    endpoint to filter an image from a public url.
+    IT SHOULD
+       1
+       1. validate the image_url query
+       2. call filterImageFromURL(image_url) to filter the image
+       3. send the resulting file in the response
+       4. deletes any files on the server on finish of the response
+    QUERY PARAMETERS
+       image_url: URL of a publicly accessible image
+    RETURNS
+      the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+    ! END @TODO1
+    */
 
-  const promiseOrTimeout = (promise: Promise<string>, time: number, exception: Symbol): Promise<any> => {
+  const callerNotAuthorized = (req: any) => {
+    return req.get("x-api-key") !== process.env.IMAGE_FILTER_X_API_KEY;
+  }
+
+  const imageUrlInvalid = (image_url: string) => {
+    return !image_url;
+  }
+
+  const promiseOrTimeout = (promise: Promise<string>, time: number, exception: Symbol): Promise<string | symbol> => {
     let timer: NodeJS.Timeout;
-    const timeout = new Promise((_, rej) => timer = setTimeout(rej, time, exception))
+    const timeout = new Promise<symbol>((_, rej) => timer = setTimeout(rej, time, exception))
     return Promise.race([promise, timeout]).finally(() => clearTimeout(timer))
   }
 
+  const advanceToNext = (next: any, timeout: number) => {
+    setTimeout(next, timeout)
+  }
+
+  const sendError = (error: unknown, timeoutError: symbol, res: any) => {
+    if (error === timeoutError) {
+      res.status(422).send({"error": "Could not process an image at the specified url."})
+    } else {
+      res.status(500).send({"error": error})
+    }
+  }
+
   const processImage = async (req: any, res: any, next: any) => {
-    if (req.get("x-api-key") !== process.env.IMAGE_FILTER_X_API_KEY) {
+    if (callerNotAuthorized(req)) {
       return res.status(403).send({"error": "Not authorized. Sad day. :("})
     }
     const image_url = req.query.image_url as string
-    if ( !image_url ) {
-      return res.status(400
-      ).send({"error": "Missing image_url query parameter."})
+    if (imageUrlInvalid(image_url)) {
+      return res.status(400).send({"error": "Missing image_url query parameter."})
     }
     const filterImageTimeoutError = Symbol()
     const timeout: number = Number(process.env.IMAGE_FILTER_PROCESSING_TIMEOUT)
     await promiseOrTimeout(filterImageFromURL(image_url), timeout, filterImageTimeoutError)
-      .then((file: any) => res.status(200).sendFile(file))
+      .then((file: string | symbol) => {
+        res.status(200).sendFile(file)
+        advanceToNext(next, timeout);
+      })
       .catch((error: any) => {
-        if (error === filterImageTimeoutError) {
-          res.status(422).send({"error": "Could not process an image at the specified url."})
-        } else {
-          res.status(500).send({"error": error})
-        }
+        sendError(error, filterImageTimeoutError, res);
       })
   }
 
-  const deleteTmpFiles = async (req: any, res: any, next: any) => {
+  const deleteTmpFiles = async (req: any, res: any) => {
     const tempDirectory = path.join(__dirname, 'util', 'tmp')
     const files = fs.readdirSync(tempDirectory).map(file => path.join(tempDirectory, file))
     await deleteLocalFiles(files)
@@ -70,14 +88,14 @@ import fs from "fs";
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
