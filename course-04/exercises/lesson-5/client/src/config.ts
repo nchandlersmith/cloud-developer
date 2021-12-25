7533ade3b060390263fffcc26ab8039308c688cf
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-12-25 14:30:24 -0600

    adds Auth0 details

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
 export const apiEndpoint = '...'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'dev-or580l07.us.auth0.com',
+  clientId: 'vsszi7NdyIW323PgId58qTkzfL8kS1ib',
   callbackUrl: 'http://localhost:3000/callback'
 }
